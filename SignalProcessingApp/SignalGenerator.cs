91796a8769f83ed619890c8749c7144763958d9d
Marek Sagan <marek.sagan@protonmail.com>
2017-10-24 01:04:48 +0200

    Added the initial GUI and the MVVM structure

---
+++ SignalProcessingApp/SignalGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalProcessingApp
+{
+    class SignalGenerator
+    {
+        int sampleRate = 8000;
+        double[] inputBuffer = new double[8000];
+        String[] outputBuffer = new String[8000];
+        double amplitude = 0.25 * short.MaxValue;
+        double frequency = 1000;
+
+
+    }
+}
---
+++ SignalProcessingApp/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,26 @@
+﻿namespace SignalProcessingApp.ViewModels
+{
+    using System.Collections.Generic;
+    using OxyPlot;
+
+    class MainWindowViewModel
+    {
+        public MainWindowViewModel()
+        {
+            this.Title = "Input signal plot";
+            this.Points = new List<DataPoint>
+            {
+                new DataPoint(0, 4),
+                new DataPoint(10, 13),
+                new DataPoint(20, 15),
+                new DataPoint(30, 16),
+                new DataPoint(40, 12),
+                new DataPoint(50, 12)
+            };
+        }
+
+        public string Title { get; private set; }
+
+        public IList<DataPoint> Points { get; private set; }
+    }
+}
