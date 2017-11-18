a072f4095ea49c1e0500f3fefa4b1c842209d80f
Marek Sagan <marek.sagan@protonmail.com>
2017-11-18 08:53:40 +0100

    Added a copyright notice

--- SignalProcessingApp/Properties/AssemblyInfo.cs
+++ SignalProcessingApp/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SignalProcessingApp")]
+[assembly: AssemblyTitle("Signal Processing App")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SignalProcessingApp")]
+[assembly: AssemblyCompany("Marek Sagan")]
+[assembly: AssemblyProduct("Signal Processing App")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
